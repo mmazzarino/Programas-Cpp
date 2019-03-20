35aba81744c0876948eb4cfe81802ffc83190972
MATHEUS MAZZARINO <matheus.mazzarino@acad.pucrs.br>
2019-03-20 19:55:20 -0300

    primeiro exemplo

---
+++ primeiroCPP/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a, b, c;
+    cout <<"Digite um numero: " << endl;
+    cin >> a;
+    cout <<"Digite um numero: " << endl;
+    cin >> b;
+    c = a+b;
+    cout << "O resultado eh: " << c << ", facil, nao?" << endl;
+    return 0;
+}
